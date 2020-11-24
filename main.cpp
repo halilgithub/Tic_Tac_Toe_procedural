95c16ccfb54412b9a895939a5f8a2978066a2152
halilgithub <halilkariyermail@gmail.com>
2020-11-24 21:03:04 +0300

    take the globals out of main and get rid of ref params in methods

--- main.cpp
+++ main.cpp
@@ -2,34 +2,33 @@
 #include <array>
 
 using namespace std;
-using boardRef = array<char,9>&;
-using constBoardRef = const array<char,9>&;
 
-void init(boardRef board);
-void displayBoard(constBoardRef board);
-int makeMove(int slot, char player);
-void changePlayer(char& currentPlayer);
-void game(boardRef board);
+array<char,9> board;
+char currentPlayer;  //X,O
+
+void init();
+void displayBoard();
+int makeMove(int slot);
+void changePlayer();
+void game();
 
 
 int main()
 {
-	array<char,9> board;
-	char currentPlayer;  //X,O
-	init(board);
-	game(board);
+	init();
+	game();
 	return 0;
 }
 
-void init(boardRef board){
+void init(){
 	board = {'1','2','3','4','5','6','7','8','9'};
 	cout << "Tic Tac Toe Console Application" << endl;
-	displayBoard(board);
+	displayBoard();
 	cout << "Two players make move in turn by entering slot number (1-9)" << endl;
 	cout << "Which player will make the first move (X,O) ?" << endl;
 }
 
-void displayBoard(constBoardRef board){
+void displayBoard(){
 	cout << " " << board[0] << " | " << board[1] << " | " << board[2]  << endl;
 	cout << "-----------" << endl;
 	cout << " " << board[3] << " | " << board[4] << " | " << board[5]  << endl;
@@ -37,17 +36,17 @@ void displayBoard(constBoardRef board){
 	cout << " " << board[6] << " | " << board[7] << " | " << board[8]  << endl;
 }
 
-int makeMove(char player){
+int makeMove(int slot){
 
 	return 0;
 }
 
-void changePlayer(char& currentPlayer){
+void changePlayer(){
 
 
 }
 
-void game(boardRef board){
+void game(){
 
 }
 
