3637aac12c0a356805d87e1b7d0460df23f625fd
halilgithub <halilkariyermail@gmail.com>
2020-11-25 04:19:53 +0300

    rv1.0

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Tic_Tac_Toe
 
-I developed this console application as a coding task.
+I developed this console application for a coding task.
+
+## Notes
 I followed a simple design and procedural approach (no need for OOP).
+I didn't add too many inline comments for easy read&review.
+
+For the board slots, I used std::array which is better than C-style array. There is no need for vector because number of board slots(9) is known in compile-time.
+
+I declared two global variables out of main. If I had declared them in main function, I  would have to pass them as ref param to the functions each time and this would be a bad idea.
+
+First version executable is ready in bin folder. I created and tested it on Windows 10 using MinGW/GCC by executing following command in project folder:
+
+g++ -lstdc++ -std=c++17 -o bin/binary_for_windows main.cpp
+
--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ array<char,9> board;
 char currentPlayer;  //X,O
 
 void init();
+bool inRange(unsigned low, unsigned high, unsigned x);
 void displayBoard();
-int makeMove(int slot);
 void changePlayer();
+bool hasWon();
 void game();
 
 
@@ -25,7 +26,11 @@ void init(){
 	cout << "Tic Tac Toe Console Application" << endl;
 	displayBoard();
 	cout << "Two players make move in turn by entering slot number (1-9)" << endl;
-	cout << "Which player will make the first move (X,O) ?" << endl;
+}
+
+bool inRange(unsigned low, unsigned high, unsigned x)
+{
+    return  ((x-low) <= (high-low));
 }
 
 void displayBoard(){
@@ -36,17 +41,78 @@ void displayBoard(){
 	cout << " " << board[6] << " | " << board[7] << " | " << board[8]  << endl;
 }
 
-int makeMove(int slot){
-
-	return 0;
+void changePlayer(){
+    if (currentPlayer == 'X')
+        currentPlayer = 'O';
+    else
+        currentPlayer = 'X';
 }
 
-void changePlayer(){
-
-
+bool hasWon(){
+    // check rows
+    if (board[0] != ' ' && board[0] == board[1] && board[1] == board[2]) return true;
+    if (board[3] != ' ' && board[3] == board[4] && board[4] == board[5]) return true;
+    if (board[6] != ' ' && board[6] == board[7] && board[7] == board[8]) return true;
+    // check columns
+    if (board[0] != ' ' && board[0] == board[3] && board[3] == board[6]) return true;
+    if (board[1] != ' ' && board[1] == board[4] && board[4] == board[7]) return true;
+    if (board[2] != ' ' && board[2] == board[5] && board[5] == board[8]) return true;
+    //check diagonal
+    if (board[0] != ' ' && board[0] == board[4] && board[4] == board[8]) return true;
+    if (board[2] != ' ' && board[2] == board[4] && board[4] == board[6]) return true;
+    return false;
 }
 
 void game(){
+    board.fill(' ');
+	char playerSelection;
+    int move;
 
+	while(1)
+	{
+        cout << "Which player will make the first move (X,O) ?" << endl;
+        cin >> playerSelection;
+
+	    if (playerSelection == 'X' || playerSelection == 'x'){
+    		currentPlayer = 'X';
+            break;
+	    }
+	    else if (playerSelection == 'O' || playerSelection == 'o'){
+            currentPlayer = 'O';
+            break;
+        }
+        else{
+            cout << "Invalid Player ! Try Again." << endl;
+            cin.clear();
+            cin.ignore(256,'\n');
+            continue;
+        }
+
+	}
+
+    for (int i = 1; i <= 9; i++)
+    {
+        displayBoard();
+        cout << "It is your turn " << currentPlayer << ". Please move (1-9):";
+        cin >> move;
+        if (cin.fail() || !inRange(1,9,move) || board[move-1] != ' '){
+            cout << "Invalid move ! Try Again." << std::endl;
+            cin.clear();
+            cin.ignore(256,'\n');
+            i--;
+            continue;
+        }
+        board[move-1] = currentPlayer;
+        if(hasWon()){
+            displayBoard();
+            cout << currentPlayer << " has Won !" << endl;
+            cout << " The End";
+            exit(0);
+        }
+        changePlayer();
+    }
+
+    displayBoard();
+    cout << "It is a DRAW. Nobody won !";
 }
 
